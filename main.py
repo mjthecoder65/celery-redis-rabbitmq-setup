8fdcaed140f306736dc35962d1739b0037e7204a
Michael Jordan <mjthecoder53161@gmail.com>
2022-10-19 14:34:14 +0900

    Added main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+import uvicorn
+from fastapi import Body, FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from celery.result import AsyncResult
+
+from config.worker import create_task, fetch_posts
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("home.html", context={"request": request})
+
+
+@app.post("/tasks", status_code=201)
+def run_task(payload = Body(...)):
+    task_type = payload["type"]
+    task = create_task.delay(int(task_type))
+    return JSONResponse({"task_id": task.id})
+
+@app.get("/tasks/{task_id}")
+def get_status(task_id):
+    task_result = AsyncResult(task_id)
+    result = {
+        "task_id": task_id,
+        "task_status": task_result.status,
+        "task_result": task_result.result
+    }
+    
+    return JSONResponse(result)
+
+
+@app.get("/posts")
+def fet_all_posts():
+    task = fetch_posts.delay()
+    return JSONResponse({"task_id": task.id})
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
